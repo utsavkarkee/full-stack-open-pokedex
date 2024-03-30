d5b02153b69ad1077f8db06247386eb7500be500
Utsav Karkee <utsavkarkee@Utsavs-MacBook-Pro.local>
2024-03-30 12:23:58 +0530

    Added health check resource

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ const PORT = process.env.PORT || 5003;
 
 app.use(express.static('dist'));
 
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+});
+
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
 
 app.listen(PORT, () => {
   // eslint-disable-next-line
